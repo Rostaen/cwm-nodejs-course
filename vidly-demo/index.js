551781547bc25d6aa1a9241b77a9845349de5173
Andrew Fillhart <afillhar@bard.edu>
2024-02-21 10:43:57 -0500

    Correted gitignore file for folders

--- vidly-demo/index.js
+++ vidly-demo/index.js
@@ -3,6 +3,10 @@ Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const users = require('./routes/user');
+const logins = require('./routes/login');
 const express = require('express');
 const app = express();
 
@@ -13,6 +17,8 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/logins', login);
 
